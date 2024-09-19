4a87ddf02636533993b498cb5a60742265d9564d
Jessica Bispo <jessicabispo.work@gmail.com>
2024-09-19 00:21:29 -0300

    adding is\-valid, will come back after some sleep

--- peg-solitaire.c
+++ peg-solitaire.c
@@ -8,4 +8,12 @@ int board[7][7] = { //ta fixo aqui mas eh melhor mudar os valores p var
     {-1, -1, 1, 1, 1, -1, -1}
 };
 
+int is_valid_move(int board[7][7], int x, int y, int dx, int dy) {
+    if (x + 2*dx < 0 || x + 2*dx >= 7 || y + 2*dy < 0 || y + 2*dy >= 7)
+        return 0;
+    return board[x][y] == 1 && 
+           board[x + dx][y + dy] == 1 && 
+           board[x + 2*dx][y + 2*dy] == 0;
+}
+
 
